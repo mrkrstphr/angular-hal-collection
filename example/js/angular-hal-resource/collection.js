df5720c8ee0a82f8013f7753f1cf148f32dcabbb
Kristopher Wilson <kristopherwilson@gmail.com>
2015-05-15 22:20:14 -0400

    Example usage page

---
+++ example/js/angular-hal-resource/collection.js
@@ -0,0 +1,101 @@
+(function () {
+  'use strict';
+
+  $collection.$inject = ['$http'];
+
+  function $collection($http) {
+
+    var CollectionFactory = function (path, resource, key) {
+      // "private" variables
+      var items;
+      var links;
+
+      // "private" methods
+      function retrieve(href) {
+        return $http.get(href).then(function (response) {
+          return new Collection(response.data);
+        });
+      }
+
+
+      function Collection(data) {
+        shallowClearAndCopy(data || {}, this);
+
+        var embeddedData = data.hasOwnProperty('_embedded') && data._embedded.hasOwnProperty(key) ?
+          data._embedded[key] : [];
+
+        items = embeddedData.map(function (item) {
+          return new resource(item);
+        });
+
+        links = data.hasOwnProperty('_links') ? data._links : [];
+      }
+
+      Object.defineProperties(Collection.prototype, {
+        items: {
+          get: function () {
+            return items;
+          }
+        },
+        links: {
+          get: function () {
+            return links;
+          }
+        }
+      });
+
+      Collection.get = function() {
+        return retrieve(path);
+      };
+
+      Collection.prototype.hasLink = function (name) {
+        return name in links && 'href' in links[name];
+      };
+
+      Collection.prototype.getLink = function (name) {
+        if (this.hasLink(name)) {
+          return links[name].href;
+        }
+      };
+
+      Collection.prototype.hasMore = function () {
+        return this.hasLink('next');
+      }
+
+      Collection.prototype.paginate = function () {
+        if (this.hasLink('next')) {
+          return retrieve(this.getLink('next'));
+        }
+      }
+
+      return Collection;
+    };
+
+    return CollectionFactory;
+  }
+
+  /**
+   * Create a shallow copy of an object and clear other fields from the destination.
+   * Borrowed from angular-resource
+   * https://github.com/angular/angular.js/blob/master/src/ngResource/resource.js
+   */
+  function shallowClearAndCopy(src, dst) {
+    dst = dst || {};
+
+    angular.forEach(dst, function(value, key) {
+      delete dst[key];
+    });
+
+    for (var key in src) {
+      if (src.hasOwnProperty(key) && key !== '_embedded' && key !== '_links') {
+        dst[key] = src[key];
+      }
+    }
+
+    return dst;
+  }
+
+  angular.module('$collection', ['ngResource'])
+    .factory('$collection', $collection);
+
+})();
---
+++ example/js/app.js
@@ -0,0 +1,69 @@
+(function () {
+  'use strict';
+
+  PlayerResource.$inject = ['$resource'];
+
+  function PlayerResource($resource) {
+    return $resource();
+  }
+
+  PlayerCollection.$inject = ['$collection', 'Player'];
+
+  function PlayerCollection($collection, Player) {
+    return $collection('/example/first.json', Player, 'players');
+  }
+
+  angular.module('sample.resources', ['ngResource', '$collection'])
+    .factory('Player', PlayerResource)
+    .factory('PlayerCollection', PlayerCollection);
+
+  PlayersController.$inject = ['PlayerCollection'];
+
+  var collection;
+
+  function PlayersController(PlayerCollection) {
+    this.players = [];
+    this.total = 0;
+
+    var that = this;
+
+    PlayerCollection.get().then(function (players) {
+      that.players = players.items;
+      that.total = players.total;
+
+      collection = players;
+    });
+  }
+
+  PlayersController.prototype.hasMore = function () {
+    return collection && collection.hasMore();
+  };
+
+  PlayersController.prototype.paginate = function () {
+    if (this.hasMore()) {
+      var that = this;
+
+      collection.paginate().then(function (players) {
+        that.players = that.players.concat(players.items);
+        collection = players;
+      });
+    }
+  };
+
+  angular.module('sample.players', ['sample.resources'])
+    .controller('PlayersController', PlayersController);
+
+  moduleConfig.$inject = ['$routeProvider'];
+
+  function moduleConfig($routeProvider) {
+    $routeProvider.when('/', {
+      controller: 'PlayersController',
+      controllerAs: 'vm',
+      templateUrl: '/example/players.html'
+    });
+  }
+
+  angular.module('sample', ['ngRoute', 'sample.players'])
+    .config(moduleConfig);
+
+})();
